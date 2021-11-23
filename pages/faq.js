34909a74f26e5c765510922f5e7f332fa09e729d
Nicolask27 <nick.kreimer@qlx.com>
2021-11-23 12:25:05 -0500

    Update faq.js

--- pages/faq.js
+++ pages/faq.js
@@ -7,7 +7,7 @@ const FaqPage = () =>
                 It aims at making it easier to discover, listen to and reward NFT music artists. See our presentation <a href="https://docs.google.com/presentation/d/1t7zirrVZ0wHu0Fx6HJeGZzsDzS0s5n4TEwT9QdC-by4/edit?usp=sharing"><u>here</u></a>.</p>
             <p><strong>Q. How does the minting work on hen radio?</strong></p>
             <p> A. Mint your audio file (preferably ogg or mp3) as usual on{' '}
-                <a href="https://www.hicetnunc.art/mint">https://www.hicetnunc.art/mint</a>
+                <a href="https://www.hicetnunc.miami/mint">https://www.hicetnunc.miami/mint</a>
             </p>
             <p><strong>Q. Is everything here also work on Hic et Nunc?</strong></p>
             <p>A. Yes, everything on hen.radio will also be available on the main Hic et Nunc
@@ -28,14 +28,14 @@ const FaqPage = () =>
                 </li>
                 <li>Clicking the link icon will copy the link to the playlist. You can share
                     this link via Twitter or email. For example:
-                    {' '}<a href="https://hen.radio/playlists/178891,11305,10159,8228">https://hen.radio/playlists/178891,11305,10159,8228</a>
+                    {' '}<a href="https://radio.hicetnunc.miam/playlists/178891,11305,10159,8228">https://hen.radio/playlists/178891,11305,10159,8228</a>
                 </li>
             </ol>
             <p>A2. To create a playlist with a cover image and visible to everybody you can message our twitter account
                @hen_radio with:</p>
             <ol>
                 <li>The link to the playlist (see above, eg:
-                    {' '}<a href="https://hen.radio/playlists/178891,11305,10159,8228">https://hen.radio/playlists/178891,11305,10159,8228</a>)
+                    {' '}<a href="https://radio.hicetnunc.miami/playlists/178891,11305,10159,8228">https://hen.radio/playlists/178891,11305,10159,8228</a>)
                 </li>
                 <li>Title of the playlist</li>
                 <li>Description</li>
@@ -45,9 +45,9 @@ const FaqPage = () =>
             <p>Playlists are a work in progress, please be patient, we will do our best to include all submissions.</p>
             <p><strong>Q. How can I contribute? How do we contact Hen.radio?</strong><br/>
                 A. <li>Ideas, praises, feedbacks? Here is <a href="https://forms.gle/sajNsHuW9GESQNaFA"><u>the form</u></a>.</li>
-                   <li>You can find the HEN Discord from this <a href="https://hicetnunc.art/about"><u>About page</u></a>, and from there find the hicathon-wg-7-2-hic-radio group.</li>
+                   <li>You can find the HEN Discord from this <a href="https://hicetnunc.miami/about"><u>About page</u></a>, and from there find the hicathon-wg-7-2-hic-radio group.</li>
                    <li>Twitter: <a href='https://twitter.com/hen_radio'>@hen_radio</a></li>
-                   <li>Github: <a href='https://github.com/henradio/hen-radio'>hen-radio</a></li>
+                   <li>Github: <a href='https://github.com/tezosmiami/hen-radio'>hen-radio</a></li>
                 </p>
         </div>
     </>;
