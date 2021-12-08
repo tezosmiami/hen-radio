50210cea030d6fda25b3cae5b9d0d4f2041987a4
Nicolask27 <nick.kreimer@qlx.com>
2021-12-08 16:41:23 -0500

    third

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
         if (!isServer) {
             // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
             config.resolve.fallback = {
-                fs: false
+                electron: false
             }
         }
     },       
