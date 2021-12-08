8ce22590417b60725d7975fad68576c18cdf2df0
Nicolask27 <nick.kreimer@qlx.com>
2021-12-08 16:50:48 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,13 +13,12 @@ module.exports = {
     },
     webpack: (config, { isServer }) => {
         if (!isServer) {
-            // set 'fs' to an empty module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-            config.node = {
-                electron: 'empty'
+            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+                fs: false
             }
         }
 
         return config;
-
-} 
-};
+    }
+        };
