0aff2872a6190f2a1e90fed443ff7a7c08230c2d
Nicolask27 <nick.kreimer@qlx.com>
2021-12-08 16:41:03 -0500

    electron fix second try

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 module.exports = {
+    
     reactStrictMode: true,
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+                fs: false
+            }
+        }
+    },       
     images: {
         domains: [
             'cloudflare-ipfs.com',
