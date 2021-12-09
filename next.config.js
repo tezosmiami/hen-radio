ab06f9dbc44740b29b6b92069dd69a85cd14a2c2
Nicolask27 <nick.kreimer@qlx.com>
2021-12-08 22:49:31 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,7 +18,8 @@ module.exports = {
                 https: false,
                 http:false,
                 os: false,
-                stream: false
+                stream: false,
+                path: false
             }
         }
 
