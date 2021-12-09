79b67bfd9833e4b129157f625a463b7fb648283a
Nicolask27 <nick.kreimer@qlx.com>
2021-12-08 21:46:14 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,8 +13,8 @@ module.exports = {
     },
     webpack: (config, { isServer }) => {
         if (!isServer) {
-            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
             config.resolve.fallback = {
+                "http": require.resolve("stream-http"),
                 fs: false
             }
         }
