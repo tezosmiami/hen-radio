61dc8502546d69f9e016804eb11f56bb37febe9e
tezosmiami <tezosmiami@gmail.com>
2022-01-07 17:57:58 -0500

    shuffle autoplay controls

--- components/views/all-tracks-view.js
+++ components/views/all-tracks-view.js
@@ -39,7 +39,7 @@ const AllTracksView = ({swrKey,fetcher}) => {
         const foundIndex = tracks.findIndex(t => t.id === Number(objkt));
         controls.initialiseTrack(tracks)(
             foundIndex !== -1 ? foundIndex : 0)();
-            shuffle && audio.play()
+            shuffle && audio.play() && controls.play();
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [tracks]);
